63740435e92a4722218bc20d69a81e077d9716a2
Asam Gnaneswar <2962820@tcs.com>
2026-09-18 17:27:00 +0530

    Updated some files

--- blocks/product-showcase/product-showcase.js
+++ blocks/product-showcase/product-showcase.js
@@ -1,23 +1,31 @@
-export default function decorate(block) {
+import { decorateBlock, loadBlock } from '../../scripts/aem.js';
+
+export default async function decorate(block) {
   block.classList.add('product-showcase');
 
   const items = [...block.children];
 
-  items.forEach((item) => {
+  await Promise.all(items.map(async (item) => {
     item.classList.add('product-showcase-item');
 
     const children = [...item.children];
+    const childBlocks = children.filter((child) => (
+      child.classList.contains('image')
+      || child.classList.contains('text')
+      || child.classList.contains('cta')
+    ));
+
+    childBlocks.forEach((child) => decorateBlock(child));
+    await Promise.all(childBlocks.map((child) => loadBlock(child)));
 
     children.forEach((child) => {
-      const content = child.firstElementChild || child;
-
-      if (content.querySelector?.('picture, img')) {
+      if (child.classList.contains('image')) {
         child.classList.add('product-showcase-image');
-      } else if (content.querySelector?.('a')) {
+      } else if (child.classList.contains('cta')) {
         child.classList.add('product-showcase-cta');
       } else {
         child.classList.add('product-showcase-description');
       }
     });
-  });
+  }));
 }
