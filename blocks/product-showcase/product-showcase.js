2352597eb8403f438e152df9c3d64585b2dd96b4
Asam Gnaneswar <2962820@tcs.com>
2026-09-21 21:15:02 +0530

    New Update in Blocks

--- blocks/product-showcase/product-showcase.js
+++ blocks/product-showcase/product-showcase.js
@@ -1,11 +1,17 @@
-export default function decorate(block) {
+import { decorateBlock, loadBlock } from '../../scripts/aem.js';
+
+export default async function decorate(block) {
   block.classList.add('product-showcase');
 
-  [...block.children].forEach((row) => {
+  await Promise.all([...block.children].map(async (row) => {
     [...row.children].forEach((column, index) => {
       if (index === 0) column.classList.add('product-showcase-image');
       if (index === 1) column.classList.add('product-showcase-text');
       if (index === 2) column.classList.add('product-showcase-cta');
     });
-  });
+
+    const children = [...row.children];
+    children.forEach((child) => decorateBlock(child));
+    await Promise.all(children.map((child) => loadBlock(child)));
+  }));
 }
