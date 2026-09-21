1d1c84c8527c7398be27a483371855d978d08252
Asam Gnaneswar <2962820@tcs.com>
2026-09-21 21:12:04 +0530

    New Update in Blocks

--- blocks/product-showcase/product-showcase.js
+++ blocks/product-showcase/product-showcase.js
@@ -1,10 +1,6 @@
 export default function decorate(block) {
   block.classList.add('product-showcase');
 
-  const firstRow = block.firstElementChild;
-  const columnCount = firstRow ? firstRow.children.length : 0;
-  block.classList.add(`product-showcase-${columnCount}-cols`);
-
   [...block.children].forEach((row) => {
     [...row.children].forEach((column, index) => {
       if (index === 0) column.classList.add('product-showcase-image');
