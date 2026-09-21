0e44ca4417ad5c49075077c7421e9253b1295197
Asam Gnaneswar <2962820@tcs.com>
2026-09-21 18:44:12 +0530

    New Update in Blocks

--- blocks/product-showcase/product-showcase.js
+++ blocks/product-showcase/product-showcase.js
@@ -1,39 +1,23 @@
-import { decorateBlock, loadBlock } from '../../scripts/aem.js';
-
-export default async function decorate(block) {
+export default function decorate(block) {
   block.classList.add('product-showcase');
 
   const items = [...block.children];
 
-  await Promise.all(items.map(async (item) => {
+  items.forEach((item) => {
     item.classList.add('product-showcase-item');
 
-    const children = [...item.children];
+    const columns = [...item.children];
 
-    const image = children.find((child) => child.classList.contains('image'));
-    const text = children.find((child) => child.classList.contains('text'));
-    const cta = children.find((child) => child.classList.contains('cta'));
-
-    const childBlocks = [image, text, cta].filter(Boolean);
-
-    childBlocks.forEach((child) => {
-      decorateBlock(child);
-    });
-
-    await Promise.all(
-      childBlocks.map((child) => loadBlock(child)),
-    );
-
-    if (image) {
-      image.classList.add('product-showcase-image');
+    if (columns[0]) {
+      columns[0].classList.add('product-showcase-image');
     }
 
-    if (text) {
-      text.classList.add('product-showcase-description');
+    if (columns[1]) {
+      columns[1].classList.add('product-showcase-description');
     }
 
-    if (cta) {
-      cta.classList.add('product-showcase-cta');
+    if (columns[2]) {
+      columns[2].classList.add('product-showcase-cta');
     }
-  }));
+  });
 }
